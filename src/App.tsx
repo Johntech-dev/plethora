1dc43c53f6cf7f89d0e5e6620a3aef0faaada429
johntech-dev <johnayomide50@gmail.com>
2023-09-25 14:26:42 +0100

    'plethora'

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Nav from './Component/Section0/Nav';
+import Header from './Component/Section1/Header';
+import Button from './Component/Section3/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
